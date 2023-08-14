5f21b65af903fe4b49f521dfa1de86530704c509
Jaws370 <jackawerner+git@gmail.com>
2023-08-14 13:45:07 -0700

    small formating changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import './App.css';
 import { ChessBoard } from './components/ChessBoard.jsx';
 
 export default function App() {
+
   return (
+
     <div id='app'>
+
       <ChessBoard />
+      
     </div>
+
   );
 }
